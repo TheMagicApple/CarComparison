359bbb8a3af4a836723597a6f8ba5331332996e6
TheMagicApple <90540132+TheMagicApple@users.noreply.github.com>
2023-02-04 15:19:14 -0800

    yessssssssssssssssssssssssssssssssssssssssssssssssssssssir

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ function search() {
 
 		if (modelStr.includes(str)) {
 			var row = table.insertRow();
-			var model = row.insertCell(0);
+            var img=row.insertCell(0);
+            img.src=carProfile[Criteria.Model]+".png";
+            console.log(img.src);
+			var model = row.insertCell(1);
 			model.innerHTML = carProfile[Criteria.Model];
-			var price = row.insertCell(1);
+			var price = row.insertCell(2);
 			price.innerHTML = "$" + carProfile[Criteria.Price];
-			var range = row.insertCell(2);
+			var range = row.insertCell(3);
 			range.innerHTML = carProfile[Criteria.Range] != -1 ? carProfile[Criteria.Range] : "Unknown";
 		}
 	}
@@ -39,6 +42,21 @@ function search() {
 
 function sort(property) {
 	// Sort data
+    if(property=="Model"){
+        document.getElementById("SortName").classList.add("selected");
+        document.getElementById("SortPrice").classList.remove("selected");
+        document.getElementById("SortRange").classList.remove("selected");
+    }
+    if(property=="Price"){
+        document.getElementById("SortName").classList.remove("selected");
+        document.getElementById("SortPrice").classList.add("selected");
+        document.getElementById("SortRange").classList.remove("selected");
+    }
+    if(property=="Range"){
+        document.getElementById("SortName").classList.remove("selected");
+        document.getElementById("SortPrice").classList.remove("selected");
+        document.getElementById("SortRange").classList.add("selected");
+    }
 	for (var i = 0; i < carData.length - 1; i++) {
 		for (var j = 0; j < carData.length - i - 1; j++) {
 			var data1 = carData[j];
@@ -69,23 +87,44 @@ function addAllResults() {
 	var table = document.getElementById("Results");
 	for (var i = 0; i < carData.length; i++) {
 		var carProfile = carData[i];
-
+        var car = document.createElement('li');
+        var img=document.createElement('img');
+        img.src=carProfile[Criteria.Model]+".png";
+        img.classList.add("carImg");
+        car.appendChild(img);
+        var name=document.createElement('div');
+        name.innerHTML=carProfile[Criteria.Model];
+        name.classList.add("carName");
+        car.appendChild(name);
+        var price=document.createElement('div');
+        price.innerHTML="$"+carProfile[Criteria.Price];
+        price.classList.add("carPrice");
+        car.appendChild(price);
+        var range=document.createElement('div');
+        range.innerHTML=carProfile[Criteria.Range]+" miles";
+        range.classList.add("carRange");
+        car.appendChild(range);
+        document.querySelector('ul').appendChild(car);
+        /*
 		var row = table.insertRow();
 		var model = row.insertCell(0);
 		model.innerHTML = carProfile[Criteria.Model];
 		var price = row.insertCell(1);
 		price.innerHTML = "$" + carProfile[Criteria.Price];
 		var range = row.insertCell(2);
-		range.innerHTML = carProfile[Criteria.Range] != -1 ? carProfile[Criteria.Range] : "Unknown";
+		range.innerHTML = carProfile[Criteria.Range] != -1 ? carProfile[Criteria.Range]+" miles" : "Unknown";
+        */
 	}
 }
 
 
 function setTableHeader() {
 	// Set Header
+    
 	var table = document.getElementById("Results");
 	table.innerHTML = ""; // Clear table
-	var header = table.createTHead();
+	/*
+    var header = table.createTHead();
 	var headerRow = header.insertRow(0);
 	var modelTag = headerRow.insertCell(0);
 	modelTag.innerHTML = "Model";
@@ -93,6 +132,7 @@ function setTableHeader() {
 	priceTag.innerHTML = "Expected Price";
 	var rangeTag = headerRow.insertCell(2);
 	rangeTag.innerHTML = "Range (mi)";
+    */
 }
 
 
